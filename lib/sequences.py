becc2a6d06ee1fc16f8b93f33933a3d2c857f5e4
Joy <joyglory213@gmail.com>
2023-11-22 12:17:05 +0300

    Completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = [0] if length >= 1 else []
+
+    while len(sequence) < length:
+        sequence.append(sequence[-1] + sequence[-2] if len(sequence) > 1 else 1)
+
+    print(sequence)
+
+    '''
+    def print_fibonacci(length):
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        sequence = [0, 1]
+
+        for i in range(2, length):
+            sequence.append(sequence[i - 1] + sequence[i - 2])
+
+        print(sequence)
+    '''
+
+
